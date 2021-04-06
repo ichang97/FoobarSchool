1d8a0107d79738f9e27861c8cf09b5048972d1db
MarkAyinomoto <markayinomoto@ADs-MacBook-Pro.local>
2021-04-06 09:47:24 +0700

    Fix bug on index page : Show index page not correct.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,8 +3,10 @@
 @section('content')
 <div class="container">
     <div class="alert alert-primary shadow">
-        <h3 class="h3 text-center">Welcome to FooBarSchool.</h3>
+        <h3 class="h3 text-center">
+        Welcome to FooBarSchool.<br />
+        Please log in first.
+        </h3>
     </div>
-
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,9 +38,16 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-dark bg-success shadow">
             <div class="container">
+                @guest
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
+                @endguest
+                @auth
+                <a class="navbar-brand" href="{{ url('home') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                @endauth
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
